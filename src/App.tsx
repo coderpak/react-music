e42bd716af1bf2bf0e687f8f614db8516b2b9fd3
coderpak <1919701172@qq.com>
2023-01-07 14:45:50 +0800

    feat: init project

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,6 @@ import { useRoutes } from 'react-router-dom'
 import routes from '@/router'
 import React from 'react'
 
-console.log(2222)
-
 function App() {
   return <div>{useRoutes(routes)}</div>
 }

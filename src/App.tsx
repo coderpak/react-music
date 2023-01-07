364d87b6b6c1c3b9f8e4f41ea88099ebd0b5432e
coderpak <1919701172@qq.com>
2023-01-07 14:20:34 +0800

    feat: log2222 测试git

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useRoutes } from 'react-router-dom'
 import routes from '@/router'
 import React from 'react'
 
-console.log(1111)
+console.log(2222)
 
 function App() {
   return <div>{useRoutes(routes)}</div>
